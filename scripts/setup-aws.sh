1f1e864bbe09ac74f6c832ba23194405be6396a3
hwalk1 <haydenjwalker1@gmail.com>
2025-08-24 13:26:52 +1000

    Test to remove BITBUCKET_BRANCH dependancy inside Actions

--- scripts/setup-aws.sh
+++ scripts/setup-aws.sh
@@ -24,10 +24,10 @@ export PATH="${AWS_HOME:-}:$PATH"
 
 if [ "$AWS_ACCESS_KEY_ID" == "" ] || [ "$AWS_SECRET_ACCESS_KEY" == "" ]; then
   # These can be used if master and prod are in different accounts or the IAM roles have different access
-  if [ "$BITBUCKET_BRANCH" == "prod" ]; then
-    export AWS_ACCESS_KEY_ID="${PROD_AWS_ACCOUNT_ACCESS_KEY_ID}"
-    export AWS_SECRET_ACCESS_KEY="${PROD_AWS_ACCOUNT_SECRET_ACCESS_KEY}"
-  else
+  # if [ "$BITBUCKET_BRANCH" == "prod" ]; then
+  #   export AWS_ACCESS_KEY_ID="${PROD_AWS_ACCOUNT_ACCESS_KEY_ID}"
+  #   export AWS_SECRET_ACCESS_KEY="${PROD_AWS_ACCOUNT_SECRET_ACCESS_KEY}"
+  # else
     export AWS_ACCESS_KEY_ID="${NON_AWS_ACCOUNT_ACCESS_KEY_ID}"
     export AWS_SECRET_ACCESS_KEY="${NON_AWS_ACCOUNT_SECRET_ACCESS_KEY}"
   fi
